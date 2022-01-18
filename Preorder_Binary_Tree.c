8df0625d7e80b4c0009c2fd0150779ca7c214e6c
Syed Shahidh Ilhan F <62804977+SYSHIL@users.noreply.github.com>
2022-01-18 17:48:00 +0530

    Create Preorder_Binary_Tree.c

---
+++ Preorder_Binary_Tree.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+struct node* newNode()
+{
+	// Allocate memory for new node
+	struct node* node
+		= (struct node*)malloc(sizeof(struct node));
+    int num;
+    printf("Enter data(-1 if no node) : ");
+    scanf("%d",&num);
+    if(num==-1)
+       return 0;
+    node->data=num;
+    printf("\nEnter data for left node of %d\n",num);
+	node->left = newNode(num);
+	printf("\nEnter data for right node of %d\n",num);
+	node->right= newNode(num);
+    return node;
+}
+void preorder(struct node* root){
+     if(root==NULL){
+         return ;
+     }
+     printf("%d ",root->data);
+     preorder(root->left);
+     preorder(root->right);
+}
+
+int main()
+{   
+	struct node* root = newNode();
+    printf("Pre order is ");
+    preorder(root);
+    inorder(root);
+	return 0;
+}
